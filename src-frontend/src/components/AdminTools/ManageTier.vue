ec70bbc81d0326863d589c3a9a9add20cc884f7b
Adrian <lilaelefant@duck.com>
2026-04-18 09:45:57 +0200

    Now correctly finalizes the invoice upon creation of the subscription.

--- src-frontend/src/components/AdminTools/ManageTier.vue
+++ src-frontend/src/components/AdminTools/ManageTier.vue
@@ -523,8 +523,12 @@ export default defineComponent({
       this.editPlanForm.loading = true;
       this.editPlanForm.error = false;
       this.editPlanForm.success = false;
+      const payload = {
+        ...this.editPlan,
+        cost: Math.round(this.editPlan.cost * 100),
+      };
       api
-        .put(`/api/admin/plans/${this.editPlan.id}/`, this.editPlan)
+        .put(`/api/admin/plans/${this.editPlan.id}/`, payload)
         .then(() => {
           this.getPlans();
           this.editPlanForm.success = true;
